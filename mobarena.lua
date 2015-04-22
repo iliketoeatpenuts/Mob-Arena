e222cf3ca1da81c12989cdae581c6a8dd723a6ab
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-04-22 12:01:25 -0400

    Update mobarena.lua

--- mobarena.lua
+++ mobarena.lua
@@ -66,23 +66,42 @@ function orange_test_spawn(data)
          OrangeLane:spawn("CREEPER");
 end
 
+function orange_test_despawn(data)
+         OrangeLane:despawn();
+end
 
 function blue_test_spawn(data)
          BlueLane:spawn("ZOMBIE");
+end
+
+function blue_test_despawn(data)
+         BlueLane:despawn();
 end
 
 function green_test_spawn(data)
          GreenLane:spawn("SKELETON");
 end
 
+function green_test_despawn(data)
+         GreenLane:despawn();
+end
+
 function yellow_test_spawn(data)
          YellowLane:spawn("WITCH");
 end
 
+function yellow_test_despawn(data)
+         YellowLane:despawn();
+end
+
 registerHook("INTERACT", "orange_test_spawn", 77, "mobarena", 831, 101, 143);
+registerHook("INTERACT", "orange_test_despawn", 77, "mobarena", 832, 101, 143);
 registerHook("INTERACT", "blue_test_spawn", 77, "mobarena", 834, 101, 143);
+registerHook("INTERACT", "blue_test_despawn", 77, "mobarena", 835, 101, 143);
 registerHook("INTERACT", "green_test_spawn", 77, "mobarena", 837, 101, 143);
+registerHook("INTERACT", "green_test_despawn", 77, "mobarena", 838, 101, 143);
 registerHook("INTERACT", "yellow_test_spawn", 77, "mobarena", 840, 101, 143);
+registerHook("INTERACT", "yellow_test_despawn", 77, "mobarena", 841, 101, 143);
 
 ----------
 --Lobby---

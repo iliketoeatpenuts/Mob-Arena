7b4d5624ecb1d70afda9dd920cb6f47dda313c37
Jesse Gauthier <Jessegauthier070296@gmail.com>
2015-05-04 12:11:27 -0400

    Need to press button to leave arena surface.

--- mobarena.lua
+++ mobarena.lua
@@ -127,11 +127,11 @@ registerHook("INTERACT", "yellow_test_despawn", 77, "mobarena", 841, 101, 143);
 --Lobby---
 ----------
 
-local surfacearena = Location:new(myWorld, 41, 67, 1);
+local surfacearenaenter = Location:new(myWorld, 41, 67, 1);
 
 function to_surface_arena(data)
        local targetPlayer = Player:new(data.player);
-       targetPlayer:teleport(surfacearena);
+       targetPlayer:teleport(surfacearenaenter);
 end
 
 registerHook("REGION_ENTER", "to_surface_arena", "mobarena-portal_surfacearena");
@@ -168,12 +168,26 @@ local sR5Done = false;
 --To know when a Round is in-progress.
 local sRoundRunning = false;
 
+local surfacesound = Location:new(myWorld, -3, 65, -1);
+local surfacearenaexit = Location:new(myWorld, 837, 97, 149);
+
 
 function surface_enter(data)
          local player = Player:new(data.player);
-         a_broadcast_npc(Overlord, player.name .. " has entered the &6Surface Arena&f!");
+         a_broadcast_npc(Overlord, player.name .. " has &ajoined &fthe struggle in the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cHead to the center of the arena to get started!", player);
 end
+
+function surface_exit1(data)
+      local targetPlayer = Player:new(data.player);
+       targetPlayer:teleport(surfacearenaexit);
+      local player = Player:new(data.player);
+       a_broadcast_npc(Overlord, player.name .. " has &cabandoned &fthe struggle in the &6Surface Arena&f!");
+end
+
+
+--Round Starting--
+
 
 function s_round1_start(data)
          local player = Player:new(data.player);
@@ -181,6 +195,7 @@ function s_round1_start(data)
          a_broadcast_npc(Overlord, player.name .. " has started &aRound 1 &fin the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cRound One has started, kill all mobs to move into round 2.", player);
          sRoundRunning = true;
+         surfacesound:playSound('PORTAL_TRIGGER', 1, 2);
       else
          a_whisper_npc(Message, "&cFinish current round before starting Round 1.", player);
    end
@@ -192,6 +207,7 @@ function s_round2_start(data)
          a_broadcast_npc(Overlord, player.name .. " has started &aRound 1 &fin the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cRound Two has started, kill all mobs to move into round 3.", player);
          sRoundRunning = true;
+         surfacesound:playSound('PORTAL_TRIGGER', 1, 2);
       else
          a_whisper_npc(Message, "&cFinish current round before starting Round 2.", player);
    end
@@ -203,6 +219,7 @@ function s_round3_start(data)
          a_broadcast_npc(Overlord, player.name .. " has started &aRound 1 &fin the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cRound Three has started, kill all mobs to move into round 4.", player);
          sRoundRunning = true;
+         surfacesound:playSound('PORTAL_TRIGGER', 1, 2);
       else
          a_whisper_npc(Message, "&cFinish current round before starting Round 3.", player);
    end
@@ -214,6 +231,7 @@ function s_round4_start(data)
          a_broadcast_npc(Overlord, player.name .. " has started &aRound 1 &fin the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cRound Four has started, kill all mobs to move into round 5.", player);
          sRoundRunning = true;
+         surfacesound:playSound('PORTAL_TRIGGER', 1, 2);
       else
          a_whisper_npc(Message, "&cFinish current round before starting Round 4.", player);
    end
@@ -225,6 +243,7 @@ function s_round5_start(data)
          a_broadcast_npc(Overlord, player.name .. " has started &aRound 1 &fin the &6Surface Arena&f!");
          a_whisper_npc(Message, "&cRound Five has started, kill all mobs to finish the arena!", player);
          sRoundRunning = true;
+         surfacesound:playSound('PORTAL_TRIGGER', 1, 2);
       else
          a_whisper_npc(Message, "&cFinish current round before starting Round 5.", player);
    end
@@ -232,11 +251,17 @@ end
 
 
 registerHook("REGION_ENTER", "surface_enter", "mobarena-arena_surface_enter");
+registerHook("INTERACT", "surface_exit1", 77, "mobarena", 30, 65, -2);
 registerHook("INTERACT", "s_round1_start", 69, "mobarena", -3, 66, -1);
 registerHook("INTERACT", "s_round2_start", 69, "mobarena", 0, 66, -4);
 registerHook("INTERACT", "s_round3_start", 69, "mobarena", 0, 66, 2);
 registerHook("INTERACT", "s_round4_start", 69, "mobarena", -6, 66, 2);
 registerHook("INTERACT", "s_round5_start", 69, "mobarena", -6, 66, -4);
+
+
+
+--Mob Spawning--
+
 
 
 local s1Zombie = Location:new(myWorld, 4.0, 65.0, 17.0);
@@ -292,13 +317,6 @@ registerHook("INTERACT", "s_round1_screeper", 69, "mobarena", -3, 66, -1);
 --ToDo:
 --Lever can not be flipped during a round, get an error message.
 --Respawn inside the arena if player dies.
---At the end of round 5 all players back to lobby.
---Cheeves
---Loot
---Coins for shop
---Signs to show whos in the lobby.
-
-
 
 ---------------------
 --Ender Arena--------
